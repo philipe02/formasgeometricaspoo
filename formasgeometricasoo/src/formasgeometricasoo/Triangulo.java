3759e428a371b948e88039c66b8fc4201a1ce065
philipe02 <philipe_02@hotmail.com>
2018-03-20 16:10:44 -0300

    adição função IsIn pro triângulo

--- formasgeometricasoo/src/formasgeometricasoo/Triangulo.java
+++ formasgeometricasoo/src/formasgeometricasoo/Triangulo.java
@@ -13,6 +13,36 @@ public class Triangulo {
         return ((a.a.getX()*a.b.getY())+(a.b.getX()*a.c.getY())+(a.c.getX()*a.a.getY())-(a.a.getY()*a.b.getX())-(a.b.getY()*a.c.getX())-(a.c.getY()*a.a.getX()));
     }       //criando função que calcula a área do triângulo
     
+    public boolean IsIn(Triangulo a, Ponto p){
+        int contador=0;     //criando contador de quantas vezes a forma será tocada
+        if(a.a.getX()< p.getX()){
+            if(a.a.getY()>a.b.getY()&&p.getY()<=a.a.getY()&&p.getY()>=a.b.getY()){
+                contador++;     //acrescentando no contador caso o valor Y no ponto A seja maior que no ponto B e se o ponto P estiver no intervalo entre os dois em Y
+            }else{
+               if(p.getY()>=a.a.getY()&&p.getY()<=a.b.getY()){
+                   contador++;      //acrescentando no contador caso o valor Y no ponto B seja maior que no ponto A e se o ponto P estiver no intervalo entre os dois em Y
+               } 
+            }
+            if(a.a.getY()>a.c.getY()&&p.getY()<=a.a.getY()&&p.getY()>=a.c.getY()){
+                contador++;     //acrescentando no contador caso o valor Y no ponto A seja maior que no ponto C e se o ponto P estiver no intervalo entre os dois em Y
+            }else{
+               if(p.getY()>=a.a.getY()&&p.getY()<=a.c.getY()){
+                   contador++;      //acrescentando no contador caso o valor Y no ponto C seja maior que no ponto A e se o ponto P estiver no intervalo entre os dois em Y
+               } 
+            }
+        }else{
+            if(a.b.getY()>a.c.getY()&&p.getY()<=a.b.getY()&&p.getY()>=a.c.getY()){
+                contador++;     //acrescentando no contador caso o valor Y no ponto B seja maior que no ponto C e se o ponto P estiver no intervalo entre os dois em Y
+            }else{
+               if(p.getY()>=a.b.getY()&&p.getY()<=a.c.getY()){
+                   contador++;      //acrescentando no contador caso o valor Y no ponto C seja maior que no ponto B e se o ponto P estiver no intervalo entre os dois em Y
+               } 
+            }   
+        }
+        return contador==1;
+    }
+    
+    
     public Ponto getA() {
         return a;
     }
